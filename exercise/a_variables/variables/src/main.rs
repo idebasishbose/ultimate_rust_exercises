638a8ef64012dc532dfbc468fc0af9df92c6503d
Debasish Bose <devbose2008@gmail.com>
2023-11-28 20:39:11 -0500

    Changes 2

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -2,8 +2,7 @@ const STARTING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
-
+    let (missiles, ready, new_var): (i32, i32, i32) = (STARTING_MISSILES, READY_AMOUNT, 1);
     println!("Firing {ready} of my {missiles} missiles...");
     println!("{} missiles left", (missiles - ready))
 }
